277069caf1bf038648c0f0f395caf05bd4af1dcb
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-17 17:46:25 +0100

    added play route and started the attack route

--- app.rb
+++ app.rb
@@ -1,18 +1,32 @@
 require 'sinatra'
 
 class Battle < Sinatra::Base
+  enable :sessions
+FULL_HITPOINTS = 100
+ATTACK = rand(1..15)
   get '/' do
     erb :index
   end
 
   post '/names' do
     p params
-    @player1 = params[:player1]
-    @player2 = params[:player2]
-    erb :play
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
 
+ get '/play' do
+   @player1 = session[:player1]
+   @player2 = session[:player2]
+   @hit_points_1 = 100
+   @hit_points_2 = 100
+   erb :play
+ end
 
+ post '/attack' do
+  @hit_points_2 = @hit_points_2 - ATTACK
+  erb :play
+ end
 
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,9 +1,13 @@
 feature 'gets the names' do
   scenario 'get player names' do
-    visit('/')
-    fill_in :player1, with: 'Pikachu'
-    fill_in :player2, with: 'Charizard'
-    click_button 'submit player names'
+    sign_in_and_play
     expect(page).to have_content 'Pikachu vs. Charizard'
+  end
+end
+
+feature 'see hit points' do
+  scenario 'see player hit points' do
+    sign_in_and_play
+    expect(page).to have_content 'Pikachu = 100'
   end
 end
